b5b345d5b9d69f0dc2b1f7fbd0690186d7a5dcaf
Edward Vidal Jr <develone@sbcglobal.net>
2017-06-02 20:03:30 +0000

    instead of reading A manually set A A =   4.0000000000000000E+000    2.0000000000000000E+000   3.0000000000000000E+001    2.0000000000000000E+001   2.0000000000000000E+001    1.5000000000000000E+001   3.5000000000000000E+001    2.8000000000000000E+001
      4.0000000000000000E+000    1.0000000000000000E+000
      4.5000000000000000E+001    1.2000000000000000E+001
      3.6000000000000000E+001    1.0000000000000000E+001
      7.0000000000000000E+001    2.0000000000000000E+001
    
    term= 1
    
    inverse of A =
      3.9999999999999147E+000   -1.9999999999999512E+000
     -2.9999999999999034E+001    1.9999999999999378E+001
      1.9999999999999261E+001   -1.4999999999999476E+001
     -3.4999999999998636E+001    2.7999999999998952E+001
    
      3.9999999999998863E+000   -9.9999999999996536E-001
     -4.4999999999998536E+001    1.1999999999999586E+001
      3.5999999999998764E+001   -9.9999999999996589E+000
     -6.9999999999997527E+001    1.9999999999999325E+001

--- invgenex.pas
+++ invgenex.pas
@@ -17,10 +17,30 @@ begin
   rewrite(output);
   writeln('program results invgenex');
   { Read matrix A}
-  iomrem(input, A[1, 1], n, n, n);
+  A[1,1] := 4.;
+  A[1,2] := 2.;
+  A[1,3] := 4.;
+  A[1,4] := 1.;
+  
+  A[2,1] := 30.;
+  A[2,2] := 20.;
+  A[2,3] := 45.;
+  A[2,4] := 12.;
+  
+  A[3,1] := 20.;
+  A[3,2] := 15.;
+  A[3,3] := 36.;
+  A[3,4] := 10.;
+  
+  A[4,1] := 35.;
+  A[4,2] := 28.;
+  A[4,3] := 70.;
+  A[4,4] := 20.;
+  //iomrem(input, A[1, 1], n, n, n);
   { Print matrix A }
   writeln;
   writeln('A =');
+
   iomwrm(output, A[1, 1], n, n, n, numdig);
   { Calculate inverse of A}
   invgen(n, n, A[1, 1], term);
